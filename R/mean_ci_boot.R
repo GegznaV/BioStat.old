74b5b47e72efc23a8c6d703059ac3d277fd2ba87
Vilmantas <GegznaV@gmail.com>
2017-11-23 19:22:40 +0200

    Functions for mean CI added

--- R/mean_ci_boot.R
+++ R/mean_ci_boot.R
@@ -1,170 +1,122 @@
 # smean.cl.boot
-ci_mean_median_boot <- function(x, conf_level = 0.95, repetitions = 2000, na.rm = TRUE, reps = FALSE) {
-        if (na.rm)
-            x <- x[!is.na(x)]
 
-           n <- length(x)
-          mean_bar <- mean(x)
-        median_bar <- median(x)
+#' Basic nonparametric bootstrap confidence interval of mean
+#'
+#' A very fast implementation of the basic nonparametric bootstrap for obtaining confidence limits for the population mean without assuming normality.
+#'
+#' The function is based on code of function \code{\link[Hmisc]{smean.cl.boot}()} from package \pkg{Hmisc}.
+#'
+#'
+#' @param x (numeric) A numeric vector from which \code{NA}s will be removed automatically.
+#' @param conf_level (number) Confidence level. Number from 0 to 1. Default 0.95.
+#' @param repetitions (integer) Number of bootstrap resamples.
+#' @param na.rm (logical) If \code{TRUE} (default), missing values (\code{NA}'s) are removed automatically.
+#' @param resampled_means (logical) If \code{TRUE}, the vector of bootstrapped means will be returned as the \code{resampled_means} attribute of the returned object.
+#' @param return_df (logical) If \code{TRUE} (default), result is returned as a data frame. If \code{FALSE} - as a matrix.
+#'
+#' @return An object (data frame or matrix) with a point estimate and confidence interval of mean.
+#'
+#' @export
+#' @examples
+#' ci_mean_boot(1:60)
+#'
+ci_mean_boot <- function(x,
+                         conf_level = 0.95,
+                         repetitions = 2000,
+                         na.rm = TRUE,
+                         resampled_means = FALSE,
+                         return_df = TRUE) {
+    checkmate::assert_numeric(x, all.missing = FALSE)
+    checkmate::assert_number(conf_level, lower = 0, upper = 1)
+    checkmate::assert_number(repetitions, lower = 1 )
 
-        if (n < 2L)
-            return(c(Mean = xbar,
-                     Lower = NA,
-                     Upper = NA
-            ))
+    if (na.rm) {
+        x <- x[!is.na(x)]
+    }
 
-        z_mean <- unlist(lapply(
-                seq_len(repetitions),
-                FUN = function(i, x, N)
-                    sum(x[sample.int(N, N, TRUE, NULL)]),
-                x = x,
-                N = n
-            )) / n
+    n <- length(x)
+    mean_bar <- mean(x)
 
-        z_median <- unlist(lapply(
-            seq_len(repetitions),
-            FUN = function(i, x, N)
-                median.default(x[sample.int(N, N, TRUE, NULL)]),
-            x = x,
-            N = n
+    if (n < 2L)
+        return(c(Mean = mean_bar,
+                 Lower = NA,
+                 Upper = NA
         ))
 
-
-        probs <- c((1 - conf_level) / 2, (1 + conf_level) / 2)
-
-        quant_median <- quantile(z_median, probs)
-        quant_mean   <- quantile(z_mean,   probs)
-
-        names(quant_mean)   <- NULL
-        names(quant_median) <- NULL
-
-        res <- matrix(c(mean_bar, quant_mean,   conf_level,
-                      median_bar, quant_median, conf_level),
-                      nrow = 2,
-                      byrow = TRUE,
-                      dimnames = list(c("Mean", "Median"),
-                                      c("estimate", "lower", "upper", "conf_level")))
-
-            # Mean = xbar,
-            #      Lower = quant[1L],
-            #      Upper = quant[2L])
+    all_means <-
+        unlist(lapply(
+            seq_len(repetitions),
+            FUN = function(i, x, n_) {
+                sum(x[sample.int(n_, n_, TRUE, NULL)])
+            },
+            x = x,
+            n_ = n
+        )) / n
 
 
+    probs <- c((1 - conf_level) / 2, (1 + conf_level) / 2)
+    quant_mean <- quantile(all_means, probs)
+    names(quant_mean) <- NULL
 
-        # if (reps)
-        #     attr(res, "reps") <- z
+    res <- matrix(c(mean_bar, quant_mean, conf_level, repetitions),
+                  nrow = 1,
+                  byrow = TRUE,
+                  dimnames = list(NULL,
+                                  c("mean", "lower", "upper", "conf_level", "repetitions")))
 
-        res
+    # Mean = xbar,
+    #      Lower = quant[1L],
+    #      Upper = quant[2L])
+
+
+    if (return_df == TRUE)
+        res <- as.data.frame(res)
+
+    if (resampled_means == TRUE)
+        attr(res, "resampled_means") <- all_means
+
+    res
 }
 
-# ci_mean_boot(x, repetitions = 5000)
 
+#' t-distribution based confidence interval of mean
+#'
+#' Student's t-distribution based confidence interval of mean
+#'
+#' @inheritParams ci_mean_boot
+#'
+#' @return An object (data frame or matrix) with a point estimate and confidence interval of mean.
+#' @export
+#'
+#' @examples
+ci_mean_t <- function(x,
+                      conf_level = 0.95,
+                      na.rm = TRUE,
+                      return_df = TRUE) {
+    checkmate::assert_numeric(x, all.missing = FALSE)
+    checkmate::assert_number(conf_level, lower = 0, upper = 1)
 
-# Hmisc::binconf
-ci_prop <- function(x, n, conf_level = 0.95,
-         method = c("wilson","exact","asymptotic","all"),
-         include.x = FALSE, include.n = FALSE,
-         return.df = FALSE)
-{
-    alpha = 1 - conf_level
-
-    ## ..modifications for printing and the addition of a
-    ##   method argument and the asymptotic interval
-    ##   and to accept vector arguments were
-    ##   made by Brad Biggerstaff on 10 June 1999
-    ##
-    ##   Modifications mane by Vilmantas Grgzna on 31 October 2017
-
-    method <- match.arg(method)
-
-    bc <- function(x, n, alpha, method)  {
-        nu1 <- 2 * (n - x + 1)
-        nu2 <- 2 * x
-        ll <- if (x > 0)
-            x/(x + qf(1 - alpha/2, nu1, nu2) * (n - x + 1))
-        else
-            0
-
-        nu1p <- nu2 + 2
-        nu2p <- nu1 - 2
-        pp <- if (x < n)
-            qf(1 - alpha/2, nu1p, nu2p)
-        else
-            1
-
-        ul <- ((x + 1) * pp)/(n - x + (x + 1) * pp)
-        zcrit <-  -qnorm(alpha/2)
-        z2 <- zcrit * zcrit
-        p <- x/n
-        cl <- (p + z2/2/n + c(-1, 1) * zcrit *
-                   sqrt((p * (1 - p) + z2/4/n)/n))/(1 + z2/n)
-
-        if (x == 1)
-            cl[1] <-  -log(1 - alpha)/n
-
-        if (x == (n - 1))
-            cl[2] <- 1 + log(1 - alpha)/n
-
-        asymp.lcl <- x/n - qnorm(1 - alpha/2) *
-            sqrt(((x/n) * (1 - x/n))/n)
-
-        asymp.ucl <- x/n + qnorm(1 - alpha/2) * sqrt(((x/n) * (1 - x/n))/n)
-        res <- rbind(c(ll, ul), cl, c(asymp.lcl, asymp.ucl))
-        res <- cbind(rep(x/n, 3), res)
-
-        ##dimnames(res) <- list(c("Exact", "Wilson", "Asymptotic"), c(
-        ## "Point Estimate", "Lower", "Upper"))
-        switch(method,
-               wilson =     res[2,  ],
-               exact =      res[1,  ],
-               asymptotic = res[3,  ],
-               all =        res,
-               res)
+    if (na.rm) {
+        x <- x[!is.na(x)]
     }
 
-    if ((length(x) != length(n)) & length(x) == 1)
-        x <- rep(x, length(n))
+    n <- length(x)
+    SD <- sd(x)
+    mean_bar <- mean(x)
 
-    if ((length(x) != length(n)) & length(n) == 1)
-        n <- rep(n, length(x))
+    t <- qt((1 - conf_level) / 2, df = (n - 1), lower.tail = FALSE)
 
-    if ((length(x) > 1 | length(n) > 1) & method == "all") {
-        method <- "wilson"
-        warning("`method = all` will not work with vectors...",
-                "setting method to `wilson`")
-    }
+    Err  <- t * SD/sqrt(n)
 
-    if (method == "all" & length(x) == 1 & length(n) == 1) {
-        mat <- bc(x, n, alpha, method)
-        dimnames(mat) <- list(c("Exact", "Wilson", "Asymptotic"),
-                              c("PointEst", "Lower", "Upper"))
-        if (include.n)
-            mat <- cbind(N = n, mat)
+    res <- matrix(c(mean_bar, mean_bar - Err, mean_bar + Err, conf_level),
+                  nrow = 1,
+                  byrow = TRUE,
+                  dimnames = list(NULL,
+                                  c("mean", "lower", "upper", "conf_level")))
 
-        if (include.x)
-            mat <- cbind(X = x, mat)
+    if (return_df == TRUE)
+        res <- as.data.frame(res)
 
-        if (return.df)
-            mat <- as.data.frame(mat)
-
-        return(mat)
-    }
-
-    mat <- matrix(ncol = 3, nrow = length(x))
-
-    for (i in 1:length(x))
-        mat[i,  ] <- bc(x[i], n[i], alpha = alpha, method = method)
-
-    dimnames(mat) <- list(rep("", dim(mat)[1]),
-                          c("PointEst", "Lower", "Upper"))
-    if (include.n)
-        mat <- cbind(N = n, mat)
-
-    if (include.x)
-        mat <- cbind(X = x, mat)
-
-    if (return.df)
-        mat <- as.data.frame(mat, row.names = NULL)
-
-    mat
+    res
 }
+
