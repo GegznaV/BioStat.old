13f209642911a0fbd20a2fbdd172784293507aad
Vilmantas <GegznaV@gmail.com>
2017-11-25 21:11:54 +0200

    Methods for `make_cld` added
    Methods `make_cld.matrix`  and `make_cld.matrix.pairwise_pval_df` and some helper functions added.

--- R/make_cld.R
+++ R/make_cld.R
@@ -25,7 +25,7 @@
 #'
 #' # Example 2: class `pairwise.htest`
 #'
-#' obj2 <- pairwise.t.test(OrchardSprays$decrease, OrchardSprays$treatment)
+#' obj2 <- with(OrchardSprays, pairwise.t.test(decrease, treatment))
 #' make_cld(obj2)
 #'
 #'
@@ -36,6 +36,7 @@
 #'                                             data = InsectSprays)
 #' make_cld(obj3)
 #' }
+#'
 #'
 #' # Example 4: class `posthocTGH`
 #'
@@ -59,6 +60,22 @@
 #'
 #' make_cld(p.adjust ~ Comparison, data = DataFrame)
 #'
+#'
+#' # Example 7: class `matrix`
+#' # (for symetric matrices of p values)
+#'
+#' # Create matrix
+#' m <- c(1.00, 0.22, 0.05, 0.00,
+#'        0.22, 1.00, 0.17, 0.01,
+#'        0.05, 0.17, 1.00, 0.22,
+#'        0.00, 0.01, 0.22, 1.00)
+#' obj7 <- matrix(m, nrow = 4)
+#' rownames(obj7) <- colnames(obj7) <- c("P", "O", "I", "U")
+#' obj7
+#'
+#' # Make cld
+#' make_cld(obj7)
+
 
 # smokers  <- c(83, 90, 129, 70)
 # patients <- c(86, 93, 136, 82)
@@ -71,6 +88,7 @@ make_cld <- function(obj, ..., alpha = 0.05) {
 }
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Helper function
 update_cld <- function(res) {
     res$MonoLetter <- gsub(" ", "_", res$MonoLetter)
     names(res) <- c("Group", "cld", "mono_cld")
@@ -82,14 +100,7 @@ update_cld <- function(res) {
 make_cld.pairwise.htest <- function(obj, ..., alpha = 0.05) {
 
     m1 <- obj$p.value
-
-    df <- data.frame(
-        gr1 = colnames(m1)[col(m1)],
-        gr2 = rownames(m1)[row(m1)],
-        p_values = c(m1))
-
-    df <- df[complete.cases(df), ]
-
+    df <- pval_matrix_to_df(m1)
     res <- rcompanion::cldList(comparison = paste0(df$gr1, " - ", df$gr2),
                                p.value    = df$p_values,
                                threshold  = alpha)
@@ -156,3 +167,59 @@ make_cld.formula <- function(obj, ..., data = NULL, alpha = 0.05) {
 
     update_cld(res)
 }
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname make_cld
+#' @export
+make_cld.matrix <- function(obj, ..., alpha = 0.05) {
+    checkmate::assert_matrix(obj, mode = "numeric")
+
+    if (is_square_matrix(obj)) {
+        if (!all(colnames(obj) == rownames(obj))) {
+            stop("Matrix is square but its column and row names does not match.")
+        }
+    } else {
+        stop("This function works with square marices only.")
+    }
+
+    obj[upper.tri(obj, diag = TRUE)] <- NA
+    df <- pval_matrix_to_df(obj)
+    make_cld.pairwise_pval_df(df)
+
+}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname make_cld
+#' @export
+make_cld.pairwise_pval_df <- function(obj, ..., alpha = 0.05) {
+    res <- rcompanion::cldList(comparison = paste0(obj$gr1, " - ", obj$gr2),
+                               p.value    = obj$p_values,
+                               threshold  = alpha)
+    update_cld(res)
+}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+is_square_matrix <- function(x) {
+    # From package `matrixcalc` version 1.0-3
+    nrow(x) == ncol(x)
+}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+is_symetric_matrix <- function(x) {
+    checkmate::assert_matrix(x, mode = "numeric")
+    if (!is_square_matrix(x)) stop("Matrix is not square")
+    sum(x == t(x)) == length(x)
+}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+pval_matrix_to_df <- function(x) {
+    if (is.null(colnames(x)))    colnames(x) <- seq_len(ncol(x))
+    if (is.null(rownames(x)))    rownames(x) <- seq_len(nrow(x))
+
+    df <- data.frame(
+        gr1 = colnames(x)[col(x)],
+        gr2 = rownames(x)[row(x)],
+        p_values = c(x))
+
+    df <- df[complete.cases(df), ]
+
+    structure(df,
+              class = c("pairwise_pval_df", "data.frame"))
+}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- tests/testthat/test-make_cld.R
+++ tests/testthat/test-make_cld.R
@@ -31,3 +31,15 @@ test_that("`make_cld.posthoc_tgh` works", {
     expect_equivalent(as.character(make_cld(obj9)$cld),
                       c("a", "ab", "bcd", "bc", "d", "cd"))
 })
+
+test_that("`make_cld.matrix` (symetric) works", {
+    m <- c(1.00, 0.22, 0.05, 0.00,
+           0.22, 1.00, 0.17, 0.01,
+           0.05, 0.17, 1.00, 0.22,
+           0.00, 0.01, 0.22, 1.00)
+    obj7 <- matrix(m, nrow = 4)
+    rownames(obj7) <- colnames(obj7) <- c("P", "O", "I", "U")
+
+    expect_equivalent(as.character(make_cld(obj7)$cld),
+                      c("a", "a", "ab", "b"))
+})
