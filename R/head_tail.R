6a19e87ba7b2940a9d53defd31d4f1aea44e1614
Vilmantas <GegznaV@gmail.com>
2018-01-03 22:28:18 +0200

    `het_tail` fixed

--- R/head_tail.R
+++ R/head_tail.R
@@ -2,6 +2,8 @@
 
 #' [!] Show several first and last rows of a data frame
 #'
+#' This function gives preview of several top and bottom rows of a data frame.
+#'
 #' @param obj A data frame.
 #' @param n (integer) Number of top and bottom rows to display.
 #' @param top (integer) Number of top rows to display.
@@ -27,23 +29,34 @@ head_tail <- function(obj,
 
     format <- match.arg(format)
 
+    obj <- as.data.frame(obj)
+
     obj_h <- head(obj, top)
     obj_t <- tail(obj, bottom)
 
     decim <- n_decimals_max(rbind(obj_h, obj_t))
 
-    df_h <- format_numbers(as.data.frame(obj_h), decim, format = format)
-    df_t <- format_numbers(as.data.frame(obj_t), decim, format = format)
+    obj_h <- format_numbers(obj_h, decim, format = format)
+    obj_t <- format_numbers(obj_t, decim, format = format)
 
-    dots  <- rep(sep, ncol(df_h))
-    space <- rep(" ", ncol(df_h))
+    df_h <- dplyr::mutate_all(obj_h, as.character)
+    df_t <- dplyr::mutate_all(obj_t, as.character)
+
+    rownames(df_h) <- rownames(obj_h)
+    rownames(df_t) <- rownames(obj_t)
+
+    dots  <- rep(sep, ncol(obj_h))
+    space <- rep(" ", ncol(obj_h))
 
     rbind(df_h, `...` = dots, df_t, `  ` = space)
 }
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Number of decimals without tailing zeros
 n_decimals <- function(x) {
-    checkmate::assert_numeric(x)
+    if (!is.numeric(x))
+        return(rep(NA, length(x)))
+
     # `scipen = 999` prebents from convertion to scientific number format
     withr::with_options(list(scipen = 999), {
         x[(x %% 1) == 0] <- ""
@@ -51,8 +64,9 @@ n_decimals <- function(x) {
         nchar(sub('(^.+\\.)(.*)(0*$)', '\\2', as.character(x)))
     })
 }
-
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Max num. of significan decimals in each column
 n_decimals_max <- function(obj) {
     sapply(obj, function(x) max(n_decimals(x)))
 }
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
